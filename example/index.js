3581780057d5541f80d20864ffc6a73bf46c8908
Aland Mariwan <dev@aland-mariwan.de>
2022-12-11 13:27:34 +0100

    auto commit

--- example/index.js
+++ example/index.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const port = 2002;
 const monitor = require('../lib/index');
-const settings= {
+const settings = {
 	// Your server name, as a flag
 	name: 'Monitoring',
 	// your server listening port
