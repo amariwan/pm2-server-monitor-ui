718dcd0f4b0ea89cf49e295b75b727071cd5080e
Aland Mariwan <dev@aland-mariwan.de>
2022-12-11 17:25:57 +0100

    auto commit

--- modules/systemInfo/cpuUsage.js
+++ modules/systemInfo/cpuUsage.js
@@ -2,6 +2,8 @@
  * It returns a promise that resolves to the CPU usage of the current process
  * @returns A promise that resolves to the CPU usage of the machine.
  */
+const osUtil = require('os-utils');
+
 const getCpuUsage = () => {
 	return new Promise((resolve) => {
 		osUtil.cpuUsage((val) => {
