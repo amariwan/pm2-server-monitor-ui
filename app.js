0cb8b714a6c4afce927e1c850377dfb616da2adb
Aland Mariwan <dev@aland-mariwan.de>
2022-12-12 14:02:42 +0100

    auto commit

--- app.js
+++ app.js
@@ -28,38 +28,21 @@ const SESSION_SECRET = uuidv4();
 //-------------------------------------------------------
 // || ======== *** SECURITY MIDDLEWARE *** ========= ||
 //-------------------------------------------------------
-
-// Session
-const session = require('express-session');
-// const sessionStore = new session.MemoryStore();
-
-app.use(
-	session({
-		name: 'session_id',
-		saveUninitialized: true,
-		resave: false,
-		rolling: false,
-		secret: SESSION_SECRET,
-		// store: sessionstore.createSessionStore(),
-		cookie: {
-			path: '/',
-			httpOnly: true,
-			maxAge: 1000 * 60 * 60 * 24,
-			sameSite: 'none',
-			secure: true,
-			HostOnly: true
-		}
-	})
-);
-const bodyParser = require('body-parser');
+const app = express();
+// using bodyParser to parse JSON bodies into JS objects
 app.use(bodyParser.json());
+/*
+ Use cookieParser and session middlewares together.
+ By default Express/Connect app creates a cookie by name 'connect.sid'.But to scale Socket.io app,
+ make sure to use cookie name 'jsessionid' (instead of connect.sid) use Cloud Foundry's 'Sticky Session' feature.
+ W/o this, Socket.io won't work if you have more than 1 instance.
+ If you are NOT running on Cloud Foundry, having cookie name 'jsessionid' doesn't hurt - it's just a cookie name.
+ */
 app.use(
 	bodyParser.urlencoded({
 		extended: true
 	})
 );
-const cookieParser = require('cookie-parser');
-app.use(cookieParser(SESSION_SECRET));
 app.use(methodOverride());
 app.use(compression());
 app.use(responseTime());
@@ -67,16 +50,43 @@ app.use(timeout('5s'));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
-app.use(express.json());
+// Add your routes here, etc.
+const haltOnTimedout = (req, res, next) => {
+	if (!req.timedout) next();
+};
+app.use(haltOnTimedout);
 app.use(morgan('combined'));
-app.set('trust proxy', true); // trust first proxy
-app.disable('x-powered-by');
+// var stats = new StatsD();
+
+// stats.socket.on('error', function(error) {
+// 	console.error(error.stack);
+// });
 // app.use(
 // 	responseTime(function(req, res, time) {
 // 		var stat = (req.method + req.url).toLowerCase().replace(/[:\.]/g, '').replace(/\//g, '_');
 // 		stats.timing(stat, time);
 // 	})
 // );
+// async function cpuData() {
+// 	try {
+// 		const data = await si.cpu();
+// 		console.log(data);
+// 		const te = await si.cpuTemperature();
+// 		console.log(te);
+// 		const wd = await si.battery();
+// 		console.log(wd);
+// 		const bb = await si.bios();
+// 		console.log(bb);
+// 		const ll = await si.fullLoad();
+// 		console.log(ll);
+// 		const ooo = await si.networkInterfaces();
+// 		console.log(ooo);
+// 	} catch (e) {
+// 		console.log(e);
+// 	}
+// }
+// cpuData();
+//
 
 //setting CSP
 
@@ -100,6 +110,41 @@ app.disable('x-powered-by');
 // 	})
 // );
 
+// This code is used to set the 'trust proxy' value to true.
+app.set('trust proxy', true); // trust first proxy
+// This code disables the x-powered-by header, which would otherwise
+// provide information about the server software that is running the
+// application.
+app.disable('x-powered-by');
+
+// Sessions allow us to Contact data on visitors from request to request
+// This keeps admins logged in and allows us to send flash messages
+// store: new FileStore(),
+app.use(
+	session({
+		name: 'session_id',
+		saveUninitialized: true,
+		resave: false,
+		rolling: false,
+		secret: SESSION_SECRET,
+		store: sessionstore.createSessionStore(),
+		cookie: {
+			path: '/',
+			httpOnly: true,
+			maxAge: 1000 * 60 * 60 * 24,
+			sameSite: 'none',
+			secure: true,
+			HostOnly: true
+		}
+	})
+);
+
+// This code parses the request body into a JSON object and assigns it to the request object
+// as req.body
+app.use(express.json());
+
+/* This is a middleware that is used to parse the body of the request. */
+// enabling CORS for all requests
 app.use(
 	cors({
 		// origin: [ `https://${hostname}:${port}` ], //frontend server localhost:8080
@@ -110,6 +155,16 @@ app.use(
 		credentials: true
 	})
 );
+
+// This code is used to parse cookies for the express app
+// The cookie parser is imported from the cookie-parser module
+// The cookie parser is used to parse cookies for the express app
+// The cookie parser is used to parse cookies to extract the session id
+app.use(cookieParser(SESSION_SECRET));
+// flash is a function that stores a message in the session and makes it available on the next request
+// flash is used to show a message after a post request
+// flash is used to display success messages or error messages
+app.use(flash());
 //-------------------------------------------------------
 // pass variables to our templates + all requests
 
