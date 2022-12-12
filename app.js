f385199755fce44db8e20faddf163242b7b64179
Aland Mariwan <dev@aland-mariwan.de>
2022-12-12 11:56:37 +0100

    auto commit

--- app.js
+++ app.js
@@ -32,15 +32,7 @@ const SESSION_SECRET = uuidv4();
 // || ======== *** SECURITY MIDDLEWARE *** ========= ||
 //-------------------------------------------------------
 const app = express();
-// using bodyParser to parse JSON bodies into JS objects
 app.use(bodyParser.json());
-/*
- Use cookieParser and session middlewares together.
- By default Express/Connect app creates a cookie by name 'connect.sid'.But to scale Socket.io app,
- make sure to use cookie name 'jsessionid' (instead of connect.sid) use Cloud Foundry's 'Sticky Session' feature.
- W/o this, Socket.io won't work if you have more than 1 instance.
- If you are NOT running on Cloud Foundry, having cookie name 'jsessionid' doesn't hurt - it's just a cookie name.
- */
 app.use(
 	bodyParser.urlencoded({
 		extended: true
