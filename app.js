4aba4f0a8f216190717e083d06ef737ca76492bb
Aland Mariwan <dev@aland-mariwan.de>
2022-12-12 11:36:53 +0100

    auto commit

--- app.js
+++ app.js
@@ -59,17 +59,12 @@ const haltOnTimedout = (req, res, next) => {
 };
 app.use(haltOnTimedout);
 app.use(morgan('combined'));
-// var stats = new StatsD();
-
-// stats.socket.on('error', function(error) {
-// 	console.error(error.stack);
-// });
-// app.use(
-// 	responseTime(function(req, res, time) {
-// 		var stat = (req.method + req.url).toLowerCase().replace(/[:\.]/g, '').replace(/\//g, '_');
-// 		stats.timing(stat, time);
-// 	})
-// );
+app.use(
+	responseTime(function(req, res, time) {
+		var stat = (req.method + req.url).toLowerCase().replace(/[:\.]/g, '').replace(/\//g, '_');
+		stats.timing(stat, time);
+	})
+);
 
 //setting CSP
 
