1aa2c3072d2003f9d4edaa3d971b17f4f5099d10
Aland Mariwan <dev@aland-mariwan.de>
2022-12-13 12:15:58 +0100

    auto commit

--- modules/PM2/control.js
+++ modules/PM2/control.js
@@ -15,6 +15,9 @@ const PM2Control = (appName, Type) => {
 				else 'serverInfo', 'success', x[0].name + ' ' + x[0].status;
 				sendMsg('serverInfo ' + 'success ' + x[0].name + ' ' + x[0].status);
 			});
+            casa "restart";
+
+
 		default:
 			break;
 	}
