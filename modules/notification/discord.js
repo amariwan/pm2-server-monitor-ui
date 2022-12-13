a7c6e1bd2cc94d5e8fab5ca2086964fa00bd55ec
Aland Mariwan <dev@aland-mariwan.de>
2022-12-13 11:37:42 +0100

    auto commit

--- modules/notification/discord.js
+++ modules/notification/discord.js
@@ -1,32 +1,65 @@
-const config = require('../../.config/config.json');
-const { Client, Events, GatewayIntentBits } = require('discord.js');
-
+const config = require('./config.json');
+const { Discord, Client, GatewayIntentBits, MessageEmbed } = require('discord.js');
+const axios = require('axios');
 const client = new Client({
 	intents: [
+		GatewayIntentBits.DirectMessages,
 		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildBans,
 		GatewayIntentBits.GuildMessages,
-		GatewayIntentBits.MessageContent,
-		GatewayIntentBits.GuildMembers
-	]
+		GatewayIntentBits.GuildBans,
+		GatewayIntentBits.GuildMembers,
+		GatewayIntentBits.GuildMessageReactions,
+		GatewayIntentBits.GuildMessageTyping,
+		GatewayIntentBits.MessageContent
+	],
+	partials: [ 'MESSAGE', 'CHANNEL', 'REACTION' ]
 });
 
-// Create a new client instance
-
-// When the client is ready, run this code (only once)
-// We use 'c' for the event parameter to keep it separate from the already defined 'client'
-client.once(Events.ClientReady, (c) => {
-	console.log(`Ready! Logged in as ${c.user.tag}`);
+client.on('ready', async () => {
+	console.log(`Logged in as ${client.user.tag}!`);
+	const user = await client.users.fetch('449846575561441280');
+	console.log(user);
+	user.send('Hey ' + user.username);
+	user.send("I'm now online");
+	client.user.setPresence({
+		activities: [ { name: `discord.js v14`, type: `WATCHING` } ],
+		status: 'dnd',
+		game: {
+			name: 'me getting developed',
+			type: 'STREAMING'
+		},
+		afk: false
+	});
 });
 
-// get message from discord ?
-client.on('message', (message) => {
-	console.log(message.content);
+client.on('warn', (err) => console.warn('[WARNING]', err));
+
+client.on('error', (err) => console.error('[ERROR]', err));
+
+client.on('disconnect', () => {
+	console.warn('Disconnected!');
+	process.exit(0);
 });
 
-client.on(Events.message, (message) => {
-	console.log(message);
+client.on('uncaughtException', (err) => {
+	console.log('Uncaught Exception: ' + err);
+	process.exit(1);
 });
 
-client.login(config.BOT_TOKEN);
+client.on('messageCreate', async (message) => {
+	if (message.author.bot) return;
+	const user = await client.users.fetch('449846575561441280');
+	var msg = message.content.toLowerCase();
+	if (msg == 'how are you?') {
+		message.author.send("I'm finde, Thank you " + user.username);
+		message.author.send('what about you ' + user.username);
+	} else {
+		let res = await axios.get(
+			`http://api.brainshop.ai/get?bid=153868&key=rcKonOgrUFmn5usX&uid=1&msg=${encodeURIComponent(msg)}`
+		);
+		message.author.send(res.data.cnt);
+	}
+});
 
-module.exports = client;
+client.login(config.token);
