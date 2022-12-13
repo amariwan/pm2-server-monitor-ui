cddf4a6169328943137476f4ca2a3cb4fb85016b
Aland Mariwan <dev@aland-mariwan.de>
2022-12-13 12:15:39 +0100

    auto commit

--- modules/PM2/control.js
+++ modules/PM2/control.js
@@ -2,14 +2,19 @@ const pm2 = require('pm2');
 
 const PM2Control = (appName, Type) => {
 	switch (Type) {
-		case "stop":
+		case 'stop':
 			pm2.stop(appName, (err, x) => {
 				if (err) return socket.emit('serverInfo', 'error', err);
 				else socket.emit('serverInfo', 'success', x[0].name + ' ' + x[0].status);
 				sendMsg('serverInfo ' + 'success ' + x[0].name + ' ' + x[0].status);
 			});
 			break;
-
+		case 'start':
+			pm2.start(appName, (err, x) => {
+				if (err) return 'serverInfo', 'error', err;
+				else 'serverInfo', 'success', x[0].name + ' ' + x[0].status;
+				sendMsg('serverInfo ' + 'success ' + x[0].name + ' ' + x[0].status);
+			});
 		default:
 			break;
 	}
