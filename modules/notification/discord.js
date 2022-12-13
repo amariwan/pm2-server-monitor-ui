c7087258ccffa935049180749703ac19d711e0c2
Aland Mariwan <dev@aland-mariwan.de>
2022-12-13 11:45:20 +0100

    auto commit

--- modules/notification/discord.js
+++ modules/notification/discord.js
@@ -1,4 +1,4 @@
-const config = require('./config.json');
+const config = require('../../.config/config.json');
 const { Discord, Client, GatewayIntentBits, MessageEmbed } = require('discord.js');
 const axios = require('axios');
 const client = new Client({
