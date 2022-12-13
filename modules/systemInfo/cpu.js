1c3f72554d19b717fca0b912dafae91a9b30c654
Aland Mariwan <dev@aland-mariwan.de>
2022-12-13 21:46:40 +0100

    auto commit

--- modules/systemInfo/cpu.js
+++ modules/systemInfo/cpu.js
@@ -2,3 +2,4 @@ const si = require('systeminformation');
 si.cpu().then((data) => console.log(data));
 si.cpuFlags().then((data) => console.log(data));
 si.cpuCache().then((data) => console.log(data));
+si.cpuCurrentSpeed().then((data) => console.log(data));
