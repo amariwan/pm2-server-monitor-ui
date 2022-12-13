7376271768df5f8247be2d635802c83650c58985
Aland Mariwan <dev@aland-mariwan.de>
2022-12-13 21:49:38 +0100

    auto commit

--- modules/systemInfo/cpu.js
+++ modules/systemInfo/cpu.js
@@ -4,8 +4,3 @@ const si = require('systeminformation');
 // si.cpuCache().then((data) => console.log(data));
 // si.cpuCurrentSpeed().then((data) => console.log(data));
 si.cpuTemperature().then((data) => console.log(data));
-const osxTemp = require('osx-temperature-sensor');
-
-let temperature = osxTemp.cpuTemperature();
-console.log('CPU-Information:');
-console.log(temperature);
