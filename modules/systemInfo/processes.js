6873e196ff2a73991d30ab35953e173356729ca0
Aland Mariwan <dev@aland-mariwan.de>
2022-12-13 22:03:57 +0100

    auto commit

--- modules/systemInfo/processes.js
+++ modules/systemInfo/processes.js
@@ -1,8 +1,13 @@
 'use strict';
 
 const si = require('systeminformation');
-si.processes().then((data) => console.log(data));
+// si.processes().then((data) => console.log(data));
 // si.currentLoad().then((data) => console.log(data)).catch((error) => console.error(error));
 // si.processLoad().then((data) => console.log(data));
 
 // module.exports =
+              setInterval(function() {
+                  si.currentLoad().then(data => {
+                      console.log(data);
+                  })
+              }, 1000
