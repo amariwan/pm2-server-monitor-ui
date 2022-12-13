bd188c91be4259f56dc4f9641135cd3fc11660aa
Aland Mariwan <dev@aland-mariwan.de>
2022-12-13 17:29:25 +0100

    auto commit

--- modules/notification/discord.js
+++ modules/notification/discord.js
@@ -15,60 +15,13 @@ const client = new Client({
 	],
 	partials: [ 'MESSAGE', 'CHANNEL', 'REACTION' ]
 });
-const data = {
-	title: 'title ~~(did you know you can have markdown here too?)~~',
-	description:
-		'this supports [named links](https://discordapp.com) on top of the previously shown subset of markdown. ```\nyes, even code blocks```',
-	url: 'https://discordapp.com',
-	color: 6828219,
-	timestamp: '2022-12-13T16:23:35.486Z',
-	footer: {
-		icon_url: 'https://cdn.discordapp.com/embed/avatars/0.png',
-		text: 'footer text'
-	},
-	thumbnail: {
-		url: 'https://cdn.discordapp.com/embed/avatars/0.png'
-	},
-	image: {
-		url: 'https://cdn.discordapp.com/embed/avatars/0.png'
-	},
-	author: {
-		name: 'author name',
-		url: 'https://discordapp.com',
-		icon_url: 'https://cdn.discordapp.com/embed/avatars/0.png'
-	},
-	fields: [
-		{
-			name: '🤔',
-			value: 'some of these properties have certain limits...'
-		},
-		{
-			name: '😱',
-			value: 'try exceeding some of them!'
-		},
-		{
-			name: '🙄',
-			value: 'an informative error should show up, and this view will remain as-is until all issues are fixed'
-		},
-		{
-			name: '<:thonkang:219069250692841473>',
-			value: 'these last two',
-			inline: true
-		},
-		{
-			name: '<:thonkang:219069250692841473>',
-			value: 'are inline fields',
-			inline: true
-		}
-	]
-};
+
 
 client.on('ready', async () => {
 	console.log(`Logged in as ${client.user.tag}!`);
 	const user = await client.users.fetch('449846575561441280');
 	user.send('Hey ' + user.username);
 	user.send("I'm now online");
-	user.(data);
 	client.user.setPresence({
 		activities: [ { name: `discord.js v14`, type: `WATCHING` } ],
 		status: 'dnd',
