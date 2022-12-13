c68501a9249a7a3d2c081af2a2def7a7707b2a12
Aland Mariwan <dev@aland-mariwan.de>
2022-12-13 14:23:47 +0100

    auto commit

--- modules/PM2/control.js
+++ modules/PM2/control.js
@@ -5,39 +5,41 @@ const PM2Control = (appName, type) => {
 	var result;
 	if (typeof appName != 'string' || appName == null) return false;
 	if (typeof type != 'string' || type == null) return false;
-	switch (type) {
-		case 'stop':
-			pm2.stop(appName, (err, x) => {
-				if (err) return (result = err);
-				else result = x[0].name + ' ' + x[0].status;
-				sendMsg('serverInfo ' + 'success ' + x[0].name + ' ' + x[0].status);
-			});
-			break;
-		case 'start':
-			pm2.start(appName, (err, x) => {
-				if (err) return (result = err);
-				else result = x[0].name + ' ' + x[0].status;
-				sendMsg('serverInfo ' + 'success ' + x[0].name + ' ' + x[0].status);
-			});
-			break;
+	if (typeof appName != 'string') {
+		switch (type) {
+			case 'stop':
+				pm2.stop(appName, (err, x) => {
+					if (err) return (result = err);
+					else result = x[0].name + ' ' + x[0].status;
+					sendMsg('serverInfo ' + 'success ' + x[0].name + ' ' + x[0].status);
+				});
+				break;
+			case 'start':
+				pm2.start(appName, (err, x) => {
+					if (err) return (result = err);
+					else result = x[0].name + ' ' + x[0].status;
+					sendMsg('serverInfo ' + 'success ' + x[0].name + ' ' + x[0].status);
+				});
+				break;
 
-		case 'restart':
-			pm2.restart(appName, (err, x) => {
-				if (err) return (result = err);
-				else result = x[0].name + ' ' + x[0].status;
-				sendMsg('serverInfo ' + 'success ' + x[0].name + ' ' + x[0].status);
-			});
-			break;
-		case 'delete':
-			pm2.delete(appName, (err, x) => {
-				if (err) return (result = err);
-				else result = x[0].name + ' ' + x[0].status;
-				sendMsg('serverInfo ' + 'success ' + x[0].name + ' ' + x[0].status);
-			});
+			case 'restart':
+				pm2.restart(appName, (err, x) => {
+					if (err) return (result = err);
+					else result = x[0].name + ' ' + x[0].status;
+					sendMsg('serverInfo ' + 'success ' + x[0].name + ' ' + x[0].status);
+				});
+				break;
+			case 'delete':
+				pm2.delete(appName, (err, x) => {
+					if (err) return (result = err);
+					else result = x[0].name + ' ' + x[0].status;
+					sendMsg('serverInfo ' + 'success ' + x[0].name + ' ' + x[0].status);
+				});
 
-			break;
-		default:
-			break;
+				break;
+			default:
+				break;
+		}
 	}
 	return result;
 };
