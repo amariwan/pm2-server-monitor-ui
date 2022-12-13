c2a34dbc7c57a7d424ea156315e579d6395d148c
Aland Mariwan <dev@aland-mariwan.de>
2022-12-13 13:28:36 +0100

    auto commit

--- modules/PM2/control.js
+++ modules/PM2/control.js
@@ -9,23 +9,22 @@ const PM2Control = (appName, type) => {
 	switch (type) {
 		case 'stop':
 			pm2.stop(appName, (err, x) => {
-				if (err) return 'serverInfo', 'error', err;
-				else 'serverInfo', 'success', x[0].name + ' ' + x[0].status;
-
+				if (err) return (result = 'serverInfo'), 'error', err;
+				else (result = 'serverInfo'), 'success', x[0].name + ' ' + x[0].status;
 				sendMsg('serverInfo ' + 'success ' + x[0].name + ' ' + x[0].status);
 			});
 			break;
 		case 'start':
 			pm2.start(appName, (err, x) => {
-				if (err) return 'serverInfo', 'error', err;
-				else 'serverInfo', 'success', x[0].name + ' ' + x[0].status;
+				if (err) return (result = 'serverInfo'), 'error', err;
+				else (result = 'serverInfo'), 'success', x[0].name + ' ' + x[0].status;
 				sendMsg('serverInfo ' + 'success ' + x[0].name + ' ' + x[0].status);
 			});
 			break;
 
 		case 'restart':
 			pm2.restart(appName, (err, x) => {
-				if (err) return socket.emit('serverInfo', 'error', err);
+				if (err) return (result = ('serverInfo', 'error', err));
 				else socket.emit('serverInfo', 'success', x[0].name + ' ' + x[0].status);
 				sendMsg('serverInfo ' + 'success ' + x[0].name + ' ' + x[0].status);
 			});
