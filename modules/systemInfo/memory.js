bc9cdf93837b4fd36a6c330d357d4d4d2cb0a0e8
Aland Mariwan <dev@aland-mariwan.de>
2022-12-15 09:44:42 +0100

    auto commit

--- modules/systemInfo/memory.js
+++ modules/systemInfo/memory.js
@@ -21,4 +21,9 @@ function memoryString(byteLen) {
 // si.mem().then((data) => console.log(data));
 // si.memLayout().then((data) => console.log(data));
 
+let arr = Array(1e6).fill('some string');
+arr.reverse();
+const used = process.memoryUsage().heapUsed / 1024 / 1024;
+console.log(`The script uses approximately ${used} MB`);
+
 module.exports = memoryString;
