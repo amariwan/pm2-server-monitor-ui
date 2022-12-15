6931e7003f50f1e6cdc33d1461a572c45441dfe5
Aland Mariwan <dev@aland-mariwan.de>
2022-12-15 09:33:46 +0100

    auto commit

--- modules/systemInfo/getInfo.js
+++ modules/systemInfo/getInfo.js
@@ -10,19 +10,19 @@ const getInfo = async () => {
 	// CPU Info
 	let info = {};
 	info.cpu = `${cpu.manufacturer} ${cpu.brand} ${cpu.speed}GHz\n`;
-	info.cores= `${cpu.cores} (${cpu.physicalCores} Physical)\n`;
+	info.cores = `${cpu.cores} (${cpu.physicalCores} Physical)\n`;
 
 	// RAM Info
 	const totalRam = Math.round(ram.total / 1024 / 1024 / 1024);
-	info += `RAM: ${totalRam}GB\n`;
+	info.ram = `${totalRam}GB\n`;
 
 	// Disk Info
 	const size = Math.round(disk.size / 1024 / 1024 / 1024);
-	info += `Disk: ${disk.vendor} ${disk.name} ${size}GB ${disk.type} (${disk.interfaceType})\n`;
+	info.disk = `${disk.vendor} ${disk.name} ${size}GB ${disk.type} (${disk.interfaceType})\n`;
 
 	//OS Info
-	info += `OS: ${os.distro} ${os.codename} (${os.platform})\n`;
-	info += `Kernel: ${os.kernel} ${os.arch}\n`;
+	info.os = `${os.distro} ${os.codename} (${os.platform})\n`;
+	info.kernel = `${os.kernel} ${os.arch}\n`;
 
 	// Node Info
 	info += `Node: v${versions.node}\n`;
