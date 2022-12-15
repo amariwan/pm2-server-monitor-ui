89b5c5a162891ee7f95957c299cfbcd63600c36d
Aland Mariwan <dev@aland-mariwan.de>
2022-12-15 13:04:31 +0100

    auto commit

--- modules/systemInfo/memoryUsage.js
+++ modules/systemInfo/memoryUsage.js
@@ -3,6 +3,7 @@ const si = require('systeminformation');
 const memoryUsage = async () => {
 	return new Promise(async (resolve, reject) => {
 		var data = await si.mem();
+		resolve((data.used/data.total)*100).toString())
 	});
 };
 
