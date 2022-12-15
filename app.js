1dbd042d2ec1c7f630fcae571cf013a240947c2a
Aland Mariwan <dev@aland-mariwan.de>
2022-12-15 08:31:54 +0100

    auto commit

--- app.js
+++ app.js
@@ -91,18 +91,18 @@ app.use(
 // 	})
 // );
 
-// app.use(methodOverride());
-// app.use(compression());
-// app.use(responseTime());
-// app.use(timeout('5s'));
-// app.use(passport.initialize());
-// app.use(passport.session());
-// app.use(flash());
-// Add your routes here, etc.
-// const haltOnTimedout = (req, res, next) => {
-// 	if (!req.timedout) next();
-// };
-// app.use(haltOnTimedout);
+app.use(methodOverride());
+app.use(compression());
+app.use(responseTime());
+app.use(timeout('5s'));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+Add your routes here, etc.
+const haltOnTimedout = (req, res, next) => {
+	if (!req.timedout) next();
+};
+app.use(haltOnTimedout);
 // app.use(morgan('combined'));
 
 //setting CSP
