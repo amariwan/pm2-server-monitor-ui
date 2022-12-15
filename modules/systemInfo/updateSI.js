59f1dd9fb04427add69962c430fdd7520df9af4a
Aland Mariwan <dev@aland-mariwan.de>
2022-12-15 18:08:38 +0100

    auto commit

--- modules/systemInfo/updateSI.js
+++ modules/systemInfo/updateSI.js
@@ -9,7 +9,7 @@ function updateSi() {
 	si.currentLoad(function(data) {
 		sysInfo.cpu = data.currentload;
 		si.mem(function(data) {
-			console.log(data);
+			console.log(data.toPrecision(3).toString() + ' %');
 			sysInfo.mem = data.active / data.total * 100;
 
 			si.cpuTemperature(function(data) {
