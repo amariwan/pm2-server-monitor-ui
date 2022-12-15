87d58e0e2f5b2788834f8622286d496e7d6c7186
Aland Mariwan <dev@aland-mariwan.de>
2022-12-15 18:03:22 +0100

    auto commit

--- modules/systemInfo/updateSI.js
+++ modules/systemInfo/updateSI.js
@@ -15,10 +15,10 @@ function updateSi() {
 			si.cpuTemperature(function(data) {
 				sysInfo.temp = data.main;
 
-				systemMonitor.system.cpu = sysInfo.cpu.toPrecision(3).toString() + ' %';
-				systemMonitor.system.mem = sysInfo.mem.toPrecision(2).toString() + ' %';
-				systemMonitor.system.temp = sysInfo.temp.toPrecision(3).toString() + ' °C';
-				systemMonitor.system.ip = ip.address();
+				system.cpu = sysInfo.cpu.toPrecision(3).toString() + ' %';
+				system.mem = sysInfo.mem.toPrecision(2).toString() + ' %';
+				system.temp = sysInfo.temp.toPrecision(3).toString() + ' °C';
+				system.ip = ip.address();
 			});
 		});
 	});
