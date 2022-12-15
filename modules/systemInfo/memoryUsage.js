b2c65ef9cb1439b647516266bffa5bde1ea419c8
Aland Mariwan <dev@aland-mariwan.de>
2022-12-15 10:24:11 +0100

    auto commit

--- modules/systemInfo/memoryUsage.js
+++ modules/systemInfo/memoryUsage.js
@@ -1,25 +1,8 @@
 'use strict';
+const si = require('systeminformation');
 
-const os = require('os');
-/**
- * Convert a byte length to a human readable string.
- * @param byteLen - The amount of memory in bytes.
- * @returns A string with the memory size in MB or GB.
- */
-function memoryString(byteLen) {
-	// get MB
-	let mem = byteLen / 1024 / 1024;
-	if (mem.toFixed() >= 1000) {
-		// Convert to GB
-		mem = (mem / 1024).toFixed(2);
-		return `${mem}GB`;
-	}
-	mem = mem.toFixed(2);
-
-	return `${mem}MB`;
-}
-// const si = require('systeminformation');
-// si.mem().then((data) => console.log(data));
-// si.memLayout().then((data) => console.log(data));
+ si.mem()
+  .then(data => var memoryString= (((data.used/data.total)*100).toString()))
+  .catch(error => res.status(404).send(siError))
 
 module.exports = memoryString;
