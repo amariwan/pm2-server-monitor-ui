5bd12f76d0c63282a9a7f4edc5f9ae1240cd238b
Aland Mariwan <dev@aland-mariwan.de>
2022-12-21 19:08:57 +0100

    auto commit

--- modules/cookie.js
+++ modules/cookie.js
@@ -1,4 +1,4 @@
-const getSessionIDCookie = (req, res) => {
+const cookie.getSessionIDCookie = (req, res) => {
 	const { headers: { cookie } } = req;
 	if (cookie) {
 		const item = cookie.split(';').reduce((res, item) => {
@@ -16,7 +16,7 @@ const getSessionIDCookie = (req, res) => {
 	} else return false;
 };
 
-const clearAllcookie = (req, res) => {
+const cookie.clearAllcookie = (req, res) => {
 	if (req == null || req.cookies == null || req.cookies.length == 0) return false;
 	cookie = req.cookies;
 	for (var prop in cookie) {
@@ -28,7 +28,7 @@ const clearAllcookie = (req, res) => {
 	return true;
 };
 // clean up sessions that go stale over time
-function session_cleanup() {
+const cookie.session_cleanup = ()=>{
 	sessionStore.all(function(err, sessions) {
 		for (var i = 0; i < sessions.length; i++) {
 			sessionStore.get(sessions[i], function() {});
