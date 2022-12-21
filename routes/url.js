b58cff20f605009a52290b2755bc651b6f906cfa
Aland Mariwan <dev@aland-mariwan.de>
2022-12-21 17:57:54 +0100

    auto commit

--- routes/url.js
+++ routes/url.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router(); // Creating a router object.
 const path = require('path');
-
+const { getSessionIDCookie } = require('../modules/cookie');
 const config_data = require('../.config/config.json');
 // Add some values to the port as the port number for http and ws
 var loginSystem = config_data.loginsystem;
@@ -14,6 +14,7 @@ if (loginSystem === false) {
 }
 /* Telling the server to serve the static files in the webUI folder. */
 router.get('/', (req, res) => {
+	console.log(getSessionIDCookie(req));
 	if (isLoginSystem) {
 		if (sessionUser) {
 			res.sendFile(path.join(__dirname, '../UI/index.html'));
