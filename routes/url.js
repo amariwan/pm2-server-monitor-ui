c03bb19aada8121ae5d709c4e50680208a331ea5
Aland Mariwan <dev@aland-mariwan.de>
2022-12-21 18:31:36 +0100

    auto commit

--- routes/url.js
+++ routes/url.js
@@ -14,7 +14,7 @@ if (loginSystem === false) {
 }
 /* Telling the server to serve the static files in the webUI folder. */
 router.get('/', (req, res) => {
-	console.log(getSessionIDCookie(req));
+	// console.log(getSessionIDCookie(req));
 	if (isLoginSystem) {
 		if (sessionUser) {
 			res.sendFile(path.join(__dirname, '../UI/index.html'));
