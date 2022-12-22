102324f91c9e8097a3b0525779ca25b0a28c067a
Aland Mariwan <dev@aland-mariwan.de>
2022-12-22 19:25:33 +0100

    auto commit

--- modules/socket/socket.js
+++ modules/socket/socket.js
@@ -35,9 +35,8 @@ io.on('connection', (socket) => {
 	var handshake = socket.handshake;
 	if (handshake.headers.cookie) {
 		var str = handshake.headers.cookie;
-		next();
 	} else {
-		next(new Error('Missing Cookies'));
+		console.log('Missing Cookies');
 	}
 	console.log('websocket server connect!');
 	// console.log(socket.handshake.headers.cookie);
