5fdd2f4ceb575de75806ea4d5322eef3a7492422
Aland Mariwan <dev@aland-mariwan.de>
2022-12-22 19:46:16 +0100

    auto commit

--- modules/socket/socket.js
+++
@@ -1,5 +0,0 @@
-const httpServer = require('../http/http');
-const socketIo = require('socket.io');
-
-const io = socketIo(httpServer); // Creating a socket.io server and attaching it to the http server.
-module.exports = io;
