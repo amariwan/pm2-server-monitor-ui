ffd2337dbf52efc1db30299cae66c23b8856eb9d
Aland Mariwan <dev@aland-mariwan.de>
2022-12-22 23:39:40 +0100

    auto commit

--- app.js
+++ app.js
@@ -202,3 +202,4 @@ const socketIo = require('socket.io');
 const io = socketIo(httpServer); // Creating a socket.io server and attaching it to the http server.
 
 app.set('socketio', io);
+var io = req.app.get('socketio');
